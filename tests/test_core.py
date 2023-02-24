f091319f0d1dba4bd69c78c2b3395e88d629db08
Prratek Ramchandani <prratek.95@gmail.com>
2023-02-24 09:47:02 -0500

    test config

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 import datetime
+import os
 
 from singer_sdk.testing import get_tap_test_class
 
@@ -8,7 +9,11 @@ from tap_firestore.tap import TapFirestore
 
 
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    "service_account_path": os.getenv(
+        "TAP_FIRESTORE_SERVICE_ACCOUNT_PATH",
+        ".secrets/service_account.json",
+    )
     # TODO: Initialize minimal tap config
 }
 
